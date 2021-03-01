05e9851661faef5479f38d5551a697d55c5c242f
Felipesorsilv <61020704+Felipesorsilv@users.noreply.github.com>
2021-03-01 14:03:22 -0600

    Add files via upload

---
+++ Cap4_Joyce/src/Caseproblem1/EventDemo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Caseproblem1;
+
+/**
+ *
+ * @author felipe
+ */
+
+import java.util.Scanner;
+        
+public class EventDemo {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        
+        
+        
+        Event ev1 = new Event();
+        System.out.println("Introduzca el Numero de evento");
+        String event = leer.nextLine();
+        System.out.println("Introduzca el numero de invitados");
+        int n = leer.nextInt();
+        Event ev2 = new Event(event, n);
+        
+        System.out.println(ev1+"\n"+ev2);
+        
+    }
+}
---
+++ Cap4_Joyce/src/Gamezone2/FiveDice.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Gamezone2;
+
+import gamezone1.Die;
+/**
+ *
+ * @author felipe
+ */
+public class FiveDice {
+    private Die[] dados;
+    
+    public FiveDice(){
+        this.dados = new Die[5];
+        for (int i = 0; i < 5; i++) {
+            this.dados[i] = new Die();
+        }
+    }
+    
+    public void lanzar(){
+        for (int i = 0; i < 5; i++) {
+            this.dados[i].lanzar();
+        }
+    }
+    
+    public Integer evaluar(){
+        Integer contador = 0;
+        Integer maximo = 0;
+        for (int i = 1; i <= 6; i++) {
+            for (int j = 0; j < 5; j++) {
+                if(this.dados[j].getNumero()==i){
+                    contador++;
+                }
+            }
+            if(contador>maximo){
+                maximo=contador;               
+            }
+            contador = 0;
+        }
+        return maximo;
+    }
+    
+    public void imprimir() {
+        for (int i = 0; i < 5; i++) {
+            System.out.print(this.dados[i].getNumero()+" ");
+        }
+        System.out.println("");
+    }
+    
+}
+
